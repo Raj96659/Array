d38c9a74ef31117762e52a9567bab0d8e8271b5e
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2023-08-13 12:52:06 +0530

    Create Accept_array.cpp

---
+++ Accept_array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int arr[5],i;
+    for(i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"\nArray elements are :";
+    for(i=0;i<5;i++)
+    {
+        cout<<" "<<arr[i];
+    }
+}
